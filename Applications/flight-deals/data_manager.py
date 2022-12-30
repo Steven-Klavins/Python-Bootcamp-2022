311e0bac2ee3e665307f807ed91c49dd25df48ae
Steven-Klavins <stevenklavins94@gmail.com>
2022-12-30 14:11:13 +0000

    added methods to update iata codes

--- Applications/flight-deals/data_manager.py
+++ Applications/flight-deals/data_manager.py
@@ -1,15 +1,36 @@
 import os
 import requests
+from flight_search import FlightSearch
 
+SHEETY_HEADERS = {
+    "Authorization": os.getenv("SHEETY_TOKEN")
+}
 
+# This class is responsible for talking to the Google Sheet.
 class DataManager:
-
-    # This class is responsible for talking to the Google Sheet.
     def get_sheet_data(self):
-        headers = {
-            "Authorization": os.getenv("SHEETY_TOKEN")
-        }
-        response = requests.get(os.getenv("SHEETY_ENDPOINT"), headers=headers)
+        response = requests.get(os.getenv("SHEETY_ENDPOINT"), headers=SHEETY_HEADERS)
         response.raise_for_status()
         data = response.json()
         print(data)
+        return data['prices']
+
+    # Update a single table cell with a new value
+    def update_cell_data(self, column_name, row_id, new_value):
+
+        params = {
+            "price": {
+                column_name: new_value
+            }
+        }
+
+        response = requests.put(f"{os.getenv('SHEETY_ENDPOINT')}/{row_id}", headers=SHEETY_HEADERS, json=params)
+        response.raise_for_status()
+
+    def update_iata_codes(self):
+        sheet_data = self.get_sheet_data()
+        flight_search = FlightSearch()
+
+        for row in sheet_data:
+            new_iata_code = flight_search.find_city_iata_code(row['city'])
+            self.update_cell_data(column_name="iataCode", row_id=(row['id']), new_value=new_iata_code)
