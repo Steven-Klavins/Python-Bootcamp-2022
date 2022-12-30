e56f4bfd25f99c3b22589cabfe19ce02a9c85a76
Steven-Klavins <stevenklavins94@gmail.com>
2022-12-30 14:10:35 +0000

    renamed method and return location data

--- Applications/flight-deals/flight_search.py
+++ Applications/flight-deals/flight_search.py
@@ -7,7 +7,7 @@ class FlightSearch:
         pass
 
     # Return a city's code
-    def find_city_code(self, city_name):
+    def find_city_iata_code(self, city_name):
         params = {
             "term": city_name,
             "location_types": "city",
@@ -20,4 +20,4 @@ class FlightSearch:
         response = requests.get(url="https://api.tequila.kiwi.com/locations/query", params=params, headers=headers)
         response.raise_for_status()
         data = response.json()
-        print(data['locations'][0]["code"])
+        return data['locations'][0]["code"]
