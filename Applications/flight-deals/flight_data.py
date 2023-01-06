81e6c23fd3ab6ca7320b8c835d90e01c90f42d79
Steven-Klavins <stevenklavins94@gmail.com>
2023-01-06 08:51:28 +0000

    check responses are successful first

--- Applications/flight-deals/flight_data.py
+++ Applications/flight-deals/flight_data.py
@@ -15,21 +15,25 @@ class FlightData:
         notification_manager = NotificationManager()
 
         # Iterate over the Google sheet data and gather the cheapest in date flights.
+        
+        if table_data:
+            for row in table_data:
+                # Gather and format the flight data.
+                best_value_flight = flight_search.find_cheapest_flight_for(city_code_from=self.city_from_iata_code,
+                                                                           city_code_to=row['iataCode'])
 
-        for row in table_data:
-            # Gather and format the flight data.
-            best_value_flight = flight_search.find_cheapest_flight_for(city_code_from=self.city_from_iata_code,
-                                                                       city_code_to=row['iataCode'])
-            lowest_current_price = int(best_value_flight['price'])
-            departure = datetime.fromisoformat(best_value_flight['local_departure'][:-1] + '+00:00').strftime(
-                '%d/%m/%Y')
+                # If the flight search had a successful response compare the data
+                if best_value_flight:
+                    lowest_current_price = int(best_value_flight['price'])
+                    departure = datetime.fromisoformat(best_value_flight['local_departure'][:-1] + '+00:00').strftime(
+                        '%d/%m/%Y')
 
-            # If a new cheaper flight is available or the flight has already departed update the Google sheet.
-            flight_has_departed = datetime.strptime(row['date'], "%d/%m/%Y").date() < datetime.now().date()
+                    # If a new cheaper flight is available or the flight has already departed update the Google sheet.
+                    flight_has_departed = datetime.strptime(row['date'], "%d/%m/%Y").date() < datetime.now().date()
 
-            if int(row['lowestPrice']) > lowest_current_price or flight_has_departed:
-                notification_manager.send_text_alert(best_value_flight)
-                data_manager.update_cell_data(column_name="lowestPrice", row_id=row['id'],
-                                              new_value=lowest_current_price)
-                data_manager.update_cell_data(column_name="date", row_id=row['id'],
-                                              new_value=departure)
+                    if int(row['lowestPrice']) > lowest_current_price or flight_has_departed:
+                        notification_manager.send_text_alert(best_value_flight)
+                        data_manager.update_cell_data(column_name="lowestPrice", row_id=row['id'],
+                                                      new_value=lowest_current_price)
+                        data_manager.update_cell_data(column_name="date", row_id=row['id'],
+                                                      new_value=departure)
