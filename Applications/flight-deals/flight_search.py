f2d54bb5a3c78c96e1ae191946e93ee33974ac1a
Steven-Klavins <stevenklavins94@gmail.com>
2023-01-06 08:51:42 +0000

    added try catch blocks

--- Applications/flight-deals/flight_search.py
+++ Applications/flight-deals/flight_search.py
@@ -17,11 +17,16 @@ class FlightSearch:
             "term": city_name,
             "location_types": "city",
         }
-
-        response = requests.get(url="https://api.tequila.kiwi.com/locations/query", params=params, headers=KIWI_HEADERS)
-        response.raise_for_status()
-        data = response.json()
-        return data['locations'][0]["code"]
+        
+        try:
+            response = requests.get(url="https://api.tequila.kiwi.com/locations/query", params=params,
+                                    headers=KIWI_HEADERS)
+            response.raise_for_status()
+            data = response.json()
+            return data['locations'][0]["code"]
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return False
 
     def find_cheapest_flight_for(self, city_code_from, city_code_to):
         params = {
@@ -34,17 +39,24 @@ class FlightSearch:
             "sort": "price"
         }
 
-        response = requests.get(url="https://api.tequila.kiwi.com/v2/search", headers=KIWI_HEADERS, params=params)
-        response.raise_for_status()
-        data = response.json()
-        cheapest_flight = data['data'][0]
-        return cheapest_flight
+        try:
+            response = requests.get(url="https://api.tequila.kiwi.com/v2/search", headers=KIWI_HEADERS, params=params)
+            response.raise_for_status()
+            data = response.json()
+            cheapest_flight = data['data'][0]
+            return cheapest_flight
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return False
 
     # Iterate over the Google sheet city names and ensure all city names have the correct IATA codes
     def update_iata_codes(self):
         data_manager = DataManager()
         sheet_data = data_manager.get_sheet_data()
 
-        for row in sheet_data:
-            new_iata_code = self.find_city_iata_code(row['city'])
-            data_manager.update_cell_data(column_name="iataCode", row_id=(row['id']), new_value=new_iata_code)
+        # If we successfully gather the sheet data gather IATA codes and make sure they're correct.
+        if sheet_data:
+            for row in sheet_data:
+                new_iata_code = self.find_city_iata_code(row['city'])
+                if new_iata_code:
+                    data_manager.update_cell_data(column_name="iataCode", row_id=(row['id']), new_value=new_iata_code)
