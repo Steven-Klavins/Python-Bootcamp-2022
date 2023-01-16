4550b3ea23bea0db8773ea5edf424058dbd974a5
Steven-Klavins <stevenklavins94@gmail.com>
2023-01-16 06:41:05 +0000

    added create playlist method

--- Applications/nostalgia-list/main.py
+++ Applications/nostalgia-list/main.py
@@ -40,34 +40,32 @@ def get_track_ids(tracks):
     track_ids = []
     for track in track_list_response:
         try:
-            track_ids.append(track["tracks"]["items"][0]['id'])
+            track_ids.append(f"spotify:track:{track['tracks']['items'][0]['id']}")
         except (IndexError, ValueError):
             pass
 
     return track_ids
 
 
-def create_playlist(playlist, playlist_ids):
-    print(playlist)
-    print(playlist_ids)
-    # TODO Create playlist
-    # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    #     client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-    #     client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-    #     redirect_uri=os.getenv("REDIRECT_URI"),
-    #     scope="user-library-read"
-    # ))
+def create_playlist_from_ids(date_selected, playlist_ids):
 
-    # playlist_name = f"Nostalgia {year[:4]}"
-    #
-    # # Create a new playlist for the user
-    #
-    # sp.user_playlist_create(user=sp.current_user()["id"],
-    #                         name=playlist_name,
-    #                         public=True,
-    #                         description=f"Top tracks for {year}")
-    #
-    # sp.playlist_add_items(sp.current_user()["id"])
+    # Authorize spotipy with a public modify scope
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        scope="playlist-modify-public"
+    ))
+
+    playlist_name = f"Nostalgia {date_selected[:4]}"
+
+    # Create a new playlist for the user.
+    new_playlist = sp.user_playlist_create(user=sp.current_user()["id"],
+                                           name=playlist_name,
+                                           public=True,
+                                           description=f"Top tracks from {date_selected}")
+    # Add the tracks.
+    sp.playlist_add_items(new_playlist["id"], playlist_ids)
 
 
 # Ask the user what year they would like to create a playlist from.
@@ -76,9 +74,6 @@ year = input("What year you would like to travel to? Type the date in this forma
 # Create the billboard top 100 url by adding the year entered.
 TOP_100_URL = f"https://www.billboard.com/charts/hot-100/{year}/"
 
-# Create a playlist name for the year entered.
-playlist_name = f"Nostalgia {year[:4]}"
-
 # Get the tracks list.
 tracks_list = get_billboard_tracks(TOP_100_URL)
 
@@ -86,4 +81,4 @@ tracks_list = get_billboard_tracks(TOP_100_URL)
 ids = get_track_ids(tracks_list)
 
 # Create the playlist
-create_playlist(playlist_name, ids)
+create_playlist_from_ids(year, ids)
