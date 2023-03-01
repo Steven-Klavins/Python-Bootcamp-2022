252213c735e4379007f5c4a587a55295fcc1028f
Steven-Klavins <stevenklavins94@gmail.com>
2023-03-01 21:50:58 +0000

    moved forms and added create user

---
+++ Web Apps/bootstrap-blog/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForms
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class CreateUserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()], render_kw={"placeholder": "Name"})
+    email = StringField("Email", validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Submit Post")
